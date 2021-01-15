b3957d8ad5a0e3473c60e7e62fbef24db9c7fe1d
Philippe HUET <phuet@redhat.com>
2021-01-15 16:57:08 +0100

    Add kubeconfig support

--- ansible/01-install-certs.sh
+++ ansible/01-install-certs.sh
@@ -7,6 +7,9 @@
   - import_role:
       name: ocp4-acme-certs
       tasks_from: main.yml
+    vars:
+      kubeconfig: "{{ lookup('env', 'KUBECONFIG') }}"
+      k8s_validate_certs: false
 #      tasks_from: certificate-install.yml
 #      tasks_from: certificate-generate.yml
 #      tasks_from: install-acme.yml
